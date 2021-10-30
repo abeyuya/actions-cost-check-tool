5153f611724eab607a7d95788d6842c718448b77
abeyuya <yuya.abe.0525@gmail.com>
2021-10-30 13:26:04 +0900

    add yargs

--- src/repoCost.ts
+++ src/repoCost.ts
@@ -1,5 +1,27 @@
-const main = async () => {
-  console.log("Hello World!");
+import * as yargs from "yargs";
+
+const argv = yargs
+  .option("owner", {
+    type: "string",
+    description: "repository owner name",
+    demandOption: true,
+  })
+  .option("repo", {
+    type: "string",
+    description: "repository name",
+    demandOption: true,
+  })
+  .option("token", {
+    type: "string",
+    description: "github access token",
+    demandOption: true,
+  })
+  .help()
+  .parseSync();
+
+const main = async (options: typeof argv) => {
+  console.log(`${options.owner}/${options.repo}`);
+  console.log(options.token);
 };
 
-main();
+main(argv);

ec214169d5391524b49e958811505aba0b3b6b42
abeyuya <yuya.abe.0525@gmail.com>
2021-10-30 13:52:08 +0900

    calc sum

--- src/repoCost.ts
+++ src/repoCost.ts
@@ -1,4 +1,5 @@
 import * as yargs from "yargs";
+import { Octokit } from "@octokit/core";
 
 const argv = yargs
   .option("owner", {
@@ -20,8 +21,43 @@ const argv = yargs
   .parseSync();
 
 const main = async (options: typeof argv) => {
-  console.log(`${options.owner}/${options.repo}`);
-  console.log(options.token);
+  const octokit = new Octokit({ auth: options.token });
+
+  const workflowsResponse = await octokit.request(
+    "GET /repos/{owner}/{repo}/actions/workflows",
+    {
+      owner: options.owner,
+      repo: options.repo,
+    }
+  );
+
+  const workflowIds = workflowsResponse.data.workflows.map((w) => w.id);
+
+  const tasks = workflowIds.map((workflowId) => {
+    return octokit.request(
+      "GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/timing",
+      {
+        owner: options.owner,
+        repo: options.repo,
+        workflow_id: workflowId,
+      }
+    );
+  });
+
+  const timingResponses = await Promise.all(tasks);
+
+  const sum = timingResponses.reduce(
+    (sum, res) => {
+      console.log(res.data.billable);
+      sum.UBUNTU += res.data.billable.UBUNTU?.total_ms || 0;
+      sum.MACOS += res.data.billable.MACOS?.total_ms || 0;
+      sum.WINDOWS += res.data.billable.WINDOWS?.total_ms || 0;
+      return sum;
+    },
+    { UBUNTU: 0, MACOS: 0, WINDOWS: 0 }
+  );
+
+  console.log(JSON.stringify({ sum }, null, 2));
 };
 
-main(argv);
+main(argv).catch(console.error);
